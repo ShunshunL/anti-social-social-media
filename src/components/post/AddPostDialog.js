eab0cab3a548493c8f9bdc631a8a6f02b51c74c0
ShunshunL <shunshunlyu@protonmail.com>
2020-06-11 20:11:47 -0700

    added serialize for caption input, finished post dialog, able to upload post and link to the current user

--- src/components/post/AddPostDialog.js
+++ src/components/post/AddPostDialog.js
@@ -1,7 +1,122 @@
 import React from 'react'
+// Import the Slate editor factory.
+import { createEditor } from 'slate'
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
+import { makeStyles, Dialog, AppBar, Toolbar, Typography, Button, Divider, Paper, Avatar, TextField, InputAdornment } from '@material-ui/core'
+import { ArrowBackIos, PinDrop } from '@material-ui/icons'
+import { UserContext } from '../../App'
+import serialize from '../../utils/serialize'
+import handleImageUpload from '../../utils/handleImageUpload'
+import { useMutation } from '@apollo/react-hooks'
+import { CREATE_POST } from '../../graphql/mutation'
 
-function AddPostDialog({ media, handleClose }) {
-  return<span>addpostdialog</span>
+const useAddPostDialogStyles = makeStyles(theme => ({
+  appBar: {
+    position: 'relative',
+    background: '#fff !important',
+    color: 'black !important',
+    display: 'flex',
+    justifyContent: 'space-between',
+    height: '54px !important'
+  },
+  toolBar: {
+    minHeight: '54px !important'
+  },
+  title: {
+    flex: 1,
+    fontWeight: 600
+  },
+  paper: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    '& > *': {
+      margin: theme.spacing(1)
+    }
+  },
+  editor: {
+    flex: 1
+  },
+  avatarLarge: {
+    width: theme.spacing(7),
+    height: theme.spacing(7)
+  },
+  input: {
+    padding: '10px !important',
+    fontSize: '14px !important'
+  },
+  root: {
+    border: '1px solid #e6e6e6',
+    marginTop: '10px !important'
+  },
+  underline: {
+    '&::before': {
+      border: 'none !important'
+    },
+    '&::after': {
+      border: 'none !important'
+    },
+    '&:hover:before': {
+      border: 'none !important'
+    }
+  }
+}))
+
+const initialValue = [
+  {
+    type: 'paragraph',
+    children: [{ text: "" }]
+  }
+]
+
+function AddPostDialog({ postImg, handleClose }) {
+  const classes = useAddPostDialogStyles()
+  // Create a Slate editor object that won't change across renders.
+  const editor = React.useMemo(() => withReact(createEditor()), [])
+  // Keep track of state for the value of the editor.
+  const [value, setValue] = React.useState(initialValue)
+  const {currentUser, currentUserId} = React.useContext(UserContext)
+  const [location, setLocation] = React.useState('')
+  const [isSubmitting, setIsSubmitting] = React.useState(false)
+  const [createPost] = useMutation(CREATE_POST)
+
+  // add an api call in this? 
+  async function handleSharePost() {
+    setIsSubmitting(true)
+    const url = await handleImageUpload(postImg)
+    const variables = {
+      userId: currentUserId,
+      location,
+      caption: serialize({ children: value }),
+      image: url
+    }
+    await createPost({ variables })
+    setIsSubmitting(false)
+    window.location.reload()
+  }
+  
+  return (
+    <Dialog open fullScreen onClose={handleClose}>
+      <AppBar className={classes.appBar}>
+        <Toolbar className={classes.toolBar}>
+          <ArrowBackIos onClick={handleClose} />
+          <Typography align="center" variant="body1" className={classes.title}>
+            New Post
+          </Typography>
+          <Button onClick={handleSharePost} color="primary" disabled={isSubmitting}>Submit</Button>
+        </Toolbar>
+      </AppBar>
+      <Divider />
+      <Paper className={classes.paper}>
+        <Avatar src={currentUser.profile_image} />
+        <Slate editor={editor} value={value} onChange={newValue => setValue(newValue)}>
+          <Editable className={classes.editor} placeholder="Write your caption..." />
+        </Slate>
+        <Avatar src={URL.createObjectURL(postImg)} className={classes.avatarLarge} variant="square" />
+      </Paper>
+      <TextField onChange={event => setLocation(event.target.value)} fullWidth placeholder="Location" InputProps={{ classes: {root: classes.root, input: classes.input, underline: classes.underline }, startAdornment: (<InputAdornment><PinDrop /></InputAdornment>)}} />
+    </Dialog>
+  )
 }
 
 export default AddPostDialog
--- src/graphql/mutation.js
+++ src/graphql/mutation.js
@@ -17,11 +17,20 @@ export const EDIT_USER_AVATAR = gql`
       affected_rows
     }
   }
-`;
+`
 
 export const EDIT_USER = gql`
   mutation editUser($id: uuid!, $name: String!, $username: String!, $bio: String!, $email: String!, $phoneNumber: String!, $website: String! ) {
   update_users(where: {id: {_eq: $id}}, _set: {bio: $bio, email: $email, name: $name, phone_number: $phoneNumber, username: $username, website: $website}) {
+    affected_rows
+  }
+}
+
+`
+
+export const CREATE_POST = gql`
+  mutation createPost($userId: uuid!, $location: String!, $image: String!, $caption: String!) {
+  insert_posts(objects: {user_id: $userId, location: $location, image: $image, caption: $caption}) {
     affected_rows
   }
 }
